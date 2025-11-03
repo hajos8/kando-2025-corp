409dcc0124aef4568d158f91184790aefa54f3a8
hajos8 <rabb.andras@gmail.com>
2025-11-03 14:21:11 +0100

    Add recordId property and display in MyFirstLightningCard component

--- force-app/main/default/lwc/myFirstLightningCard/myFirstLightningCard.js
+++ force-app/main/default/lwc/myFirstLightningCard/myFirstLightningCard.js
@@ -1,3 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class MyFirstLightningCard extends LightningElement {}
+export default class MyFirstLightningCard extends LightningElement {
+    @api recordId;
+
+    connectedCallback() {
+        console.log('Record Id: ' + this.recordId);
+    }
+
+    
+}
