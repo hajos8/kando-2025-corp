18024083c8a3944932c8eb6eed5d23b6e70cbfd2
hajos8 <rabb.andras@gmail.com>
2025-11-03 14:34:38 +0100

    Refactor recordId property in MyFirstLightningCard component to use getter and setter for improved encapsulation and logging

--- force-app/main/default/lwc/myFirstLightningCard/myFirstLightningCard.js
+++ force-app/main/default/lwc/myFirstLightningCard/myFirstLightningCard.js
@@ -1,11 +1,26 @@
 import { LightningElement, api } from 'lwc';
 
 export default class MyFirstLightningCard extends LightningElement {
-    @api recordId;
+    _recordId;
+    @api get recordId(){
+        console.log('Record Id get: ' + this._recordId);
+        return this._recordId;
+    }
+
+    set recordId(value){
+        this._recordId = value;
+        console.log('Record Id set: ' + this._recordId);
+    }
 
     connectedCallback() {
         console.log('Record Id: ' + this.recordId);
     }
 
-    
+    renderedCallback() {
+        console.log('Record Id rendered: ' + this.recordId);
+    }
+
+    disconnectedCallback() {
+        console.log('Record Id disconnected: ' + this.recordId);
+    }   
 }
